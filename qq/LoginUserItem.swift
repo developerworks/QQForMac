7fb764e7c77cd6a68ae15f051ae6ac6b024fc6ee
developerworks <developerworks@163.com>
2018-05-01 17:58:03 +0800

    add avatar images

---
+++ qq/LoginUserItem.swift
@@ -0,0 +1,18 @@
+//
+//  LoginUserItem.swift
+//  qq
+//
+//  Created by hezhiqiang on 2018/5/1.
+//  Copyright © 2018年 Totorotec. All rights reserved.
+//
+
+import Cocoa
+
+class LoginUserItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ qq/LoginUserItemModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginUserItemModel.swift
+//  qq
+//
+//  Created by hezhiqiang on 2018/5/1.
+//  Copyright © 2018年 Totorotec. All rights reserved.
+//
+
+import Foundation
